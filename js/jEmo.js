ff0f9838ea1569cae757a4d6db005daa289e5a26
李庆芳 <867259206@qq.com>
2016-09-09 19:14:35 +0800

    complete init

--- js/jEmo.js
+++ js/jEmo.js
@@ -1,17 +1,39 @@
 /**
  * Created by 李庆芳 on 2016/9/4.
  */
-;(function ( $, window, undefined ) {
-        'use strict';
+;(function ($, window, undefined) {
 
-    $.jEmo=function(options,element){
-        this.$el = $( element );
-        this._init( options );
+    'use strict';
+
+    $.jEmo = function (options) {
+        this.target = $.jEmo.defaults.target;
+        this._init(options);
     }
+
     // the options
     $.jEmo.defaults = {
         container: document.getElementById('jEmo-emo'),
         target: document.getElementById('jEmo-textarea'),
+        position: 'down',
+        emotions:emotions.json
     };
 
-} )( jQuery, window );
+    $.jEmo.prototype = {
+        _init: function (options) {
+            this.options = $.extend(true, {}, $.jEmo.defaults, options);
+            this._loadEvents();
+        },
+
+        _loadEvents: function () {
+            this.target.on('click.jEmo', function (event) {
+                var target = $(this),
+                    doc = document,
+                    container = doc.createElement('ul');
+                target.appendChild(container);
+
+            });
+        }
+    }
+
+
+})(jQuery, window);
